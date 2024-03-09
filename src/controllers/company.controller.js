ab02e56d20fbf8afe9a4e701891c0f273b254f0d
Ashutosh <ashutoshm610@gmail.com>
2024-03-10 01:08:03 +0530

    controller added

--- src/controllers/company.controller.js
+++ src/controllers/company.controller.js
@@ -1,7 +1,75 @@
+const Company = require('../models/tbl_company');
 const Building = require('../models/tbl_building');
 
-module.exports.add = (req, res) => {};
-module.exports.get_all = (req, res) => {};
-module.exports.get = (req, res) => {};
-module.exports.update = (req, res) => {};
-module.exports.delete = (req, res) => {};
+module.exports.add = async (req, res) => {
+  const { name, description } = req.body;
+  if (!name) throw new ApiError(301, 'Provide the category name');
+
+  try {
+    const company = await Company.create({ name, description });
+    res.status(200).json(new ApiResponse(200, company, 'company created'));
+  } catch (error) {
+    throw new ApiError(500, 'Internal Server Error');
+  }
+};
+
+module.exports.get_all = async (req, res) => {
+  try {
+    const company = await Company.find({ deleted: false });
+    if (!company.length) throw new ApiError(404, 'company not found');
+    res
+      .status(200)
+      .json(new ApiResponse(200, company, 'company found successfully'));
+  } catch (error) {
+    throw new ApiError(500, 'Internal Server Error');
+  }
+};
+
+module.exports.get = async (req, res) => {
+  const { _id } = req.params;
+  try {
+    const company = await Company.findOne({ _id, deleted: false });
+    if (!company) throw new ApiError(404, 'company not found');
+    res.status(200).json(new ApiResponse(200, company, 'company Found'));
+  } catch (error) {
+    throw new ApiError(500, 'Internal Server Error');
+  }
+};
+
+module.exports.update = async (req, res) => {
+  const { _id, name, description } = req.body;
+  if (!name) throw new ApiError(301, 'Provide the company name');
+
+  try {
+    const updated_company = await Company.findByIdAndUpdate(
+      _id,
+      { name, description },
+      { new: true }
+    );
+    if (!updated_company) throw new ApiError(404, 'Company not found');
+    res
+      .status(200)
+      .json(
+        new ApiResponse(200, updated_company, 'Company updated successfully')
+      );
+  } catch (error) {
+    throw new ApiError(500, 'Internal Server Error');
+  }
+};
+
+module.exports.delete = async (req, res) => {
+  const { _id } = req.params;
+  try {
+    const deleted_company = await Company.findByIdAndUpdate(
+      _id,
+      { deleted: true, deleted_by: req.user_id },
+      { new: true }
+    );
+    if (!deleted_company) throw new ApiError(404, 'Company not found');
+    res
+      .status(200)
+      .json(new ApiResponse(200, true, 'Company deleted successfully'));
+  } catch (error) {
+    throw new ApiError(500, 'Internal Server Error');
+  }
+};
--- src/controllers/vendor.controller.js
+++ src/controllers/vendor.controller.js
@@ -1,7 +1,114 @@
 const Vendor = require('../models/tbl_vendor');
+const ApiResponse = require('../utils/Api.response');
 
-module.exports.add = (req, res) => {};
-module.exports.get_all = (req, res) => {};
-module.exports.get = (req, res) => {};
-module.exports.update = (req, res) => {};
-module.exports.delete = (req, res) => {};
+module.exports.add = (req, res) => {
+  try {
+    const { vendor_name, phone_nu, aadhaar_nu, pan_nu, food_license_nu } =
+      req.body;
+
+    if (
+      [vendor_name, phone_nu, aadhaar_nu, pan_nu, food_license_nu].some(
+        field.trim === ''
+      )
+    ) {
+      throw new ApiError(401, "Value can't be null");
+    }
+
+    const vendor = new Vendor.create({
+      vendor_name,
+      phone_nu,
+      aadhaar_nu,
+      pan_nu,
+      food_license_nu,
+    });
+
+    if (!vendor) {
+      throw new ApiError(401, 'Vendor not created');
+    }
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, vendor, 'Vendor created successfully'));
+  } catch (error) {
+    throw new ApiError(501, 'Internal server error');
+  }
+};
+
+module.exports.get_all = async (req, res) => {
+  try {
+    const Vendors = await Vendor.find({ deleted: false });
+
+    if (!Vendors.length) {
+      throw new ApiError(404, 'Vendors not found');
+    }
+    return res
+      .status(200)
+      .json(new ApiResponse(200, Ve, 'Fetched all Vendors'));
+  } catch (error) {
+    throw new ApiError(501, error.message);
+  }
+};
+module.exports.get = async (req, res) => {
+  try {
+    const { _id } = req.params;
+    const vendor = await Vendor.findOne({ _id });
+    if (!vendor) {
+      throw new ApiError(404, 'vendor not found');
+    }
+    return res
+      .status(200)
+      .json(new ApiResponse(200, vendor, 'vendor found succesfully'));
+  } catch (error) {
+    throw new ApiError(501, error.message);
+  }
+};
+module.exports.update = (req, res) => {
+  const { vendor_name, phone_nu, aadhaar_nu, pan_nu, food_license_nu } =
+    req.body;
+
+  if (
+    [vendor_name, phone_nu, aadhaar_nu, pan_nu, food_license_nu].some(
+      field.trim === ''
+    )
+  ) {
+    throw new ApiError(401, "Value can't be null");
+  }
+
+  const updated_item = Vendor.findByIdAndUpdate(
+    _id,
+    { vendor_name, phone_nu, aadhaar_nu, pan_nu, food_license_nu },
+    { new: true }
+  );
+
+  if (!updated_item) {
+    throw new ApiError(404, 'Vendor not found');
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updated_item, 'Vendor updated successfully'));
+};
+
+module.exports.delete = async (req, res) => {
+  try {
+    const { _id } = req.params;
+    const deleted_item = await Vendor.findByIdAndUpdate(
+      _id,
+      { deleted: true },
+      { new: true }
+    );
+
+    if (!deleted_item) {
+      throw new ApiError(404, 'Vendor not found');
+    }
+    if (!deleted_item.deleted) {
+      throw new ApiError(501, 'Internal server error');
+    }
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, true, 'Deleted sucessfully'));
+  } catch (error) {
+    throw new ApiError(501, error.message);
+  }
+};
